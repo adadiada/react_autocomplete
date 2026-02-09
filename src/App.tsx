9d695d0faad30cacaa1a5042235cb45cc0d7f7a3
Hanna Yatskiv <hanna.ada7@gmail.com>
2026-02-09 16:26:37 +0300

    Message

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import './App.scss';
 import { Person } from './types/Person';
 import { peopleFromServer } from './data/people';
 
 import { Autocomplete } from './Autocomplete';
+import { setTimeout } from 'timers/promises';
+import debounce from 'lodash.debounce';
+
+// function debounce(callback: Function, delay: number) {
+//   let timeId = 0;
+
+//   return (...arg) => {
+//     window.clearTimeout(timeId);
+
+//     timeId = window.setTimeout(() => {
+//       callback(...arg);
+//     }, delay);
+//   };
+// }
 
 export const App: React.FC = () => {
   const [query, setQuery] = useState('');
   const [selected, setSelected] = useState<Person | null>(null);
+  const [applyQuery, setApplyQuery] = useState('');
+
+  const handleSelectePerson = (person: Person) => {
+    setSelected(person);
+    setQuery(person);
+  };
+
+  const applieQuery = useCallback(debounce(setApplyQuery, 300), []);
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
+    applieQuery(event.target.value);
   };
 
   const filteredPeople = useMemo(() => {
-    return people.filter(person =>
-      person.name.toLowerCase().includes(query.toLowerCase()),
+    return peopleFromServer.filter(person =>
+      person.name.toLowerCase().includes(applyQuery.toLowerCase()),
     );
-  }, [query, people]);
+  }, [applyQuery]);
+
+  const titleField = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (titleField.current) {
+      titleField.current.focus();
+    }
+  }, []);
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {` selected ? ${selected.name} (${selected.born} - ${selected.died}) : 'No selected person'`}
+          {selected
+            ? `${selected.name} (${selected.born} - ${selected.died})`
+            : 'No selected person'}
         </h1>
 
         <div className="dropdown is-active">
           <div className="dropdown-trigger">
             <input
               type="text"
+              ref={titleField}
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
@@ -38,7 +72,7 @@ export const App: React.FC = () => {
             />
           </div>
         </div>
-      <Autocomplete people={filteredPeople} onSelect={setSelected} />
+        <Autocomplete people={filteredPeople} onSelected={handleSelectePerson} />
         <div
           className="
             notification
--- src/Autocomplete.tsx
+++ src/Autocomplete.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Person } from './types/Person';
 
 type Props = {
   people: Person[];
-  delay?: number;
   onSelected?: (p: Person | null) => void;
 };
 
 export const Autocomplete: React.FC<Props> = React.memo(({
   people,
-  delay = 300,
-  onSelected = () = {},
+  onSelected = () => {},
 }) => {
   return (
     <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-      {people.map((person) =>
+      {people.map((person) => (
         <div
           key={person.id}
-          onClick={() => onSelected(people)}
+          onClick={() => onSelected(person)}
           className="dropdown-item"
           data-cy="suggestion-item"
           >
             <p className="has-text-link">{person.name}</p>
           </div>
-      )};
+      ))}
 
-    </div>
-  )
-});
+      </div>
+    );
+  },
+);
